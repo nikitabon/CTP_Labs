8044177317113710323363f8168d854533273edf
nikitabon <80147415+nikitabon@users.noreply.github.com>
2021-04-28 07:58:56 +0000

    Update Lab_4.cpp

--- Lab_4.cpp
+++ Lab_4.cpp
@@ -1,6 +1,7 @@
 /* Варіант 1. Написати програму, яка перетворює масив таким чином, щоб спочатку розташовувалися нульові елементи, а потім всі інші. */
 
 #include<iostream>
+#include<cstdlib>
 
 void restore_array(int * array, int array_size);
 void show_array(int * array, int array_size);
@@ -9,16 +10,46 @@ int main()
 {
 	int * array;
 	int array_size;
+	char simbols[50];
+	
+	up:;
 	
 	std::cout << "How many elements the array should store ? : ";
-	std::cin >> array_size;
+	std::cin >> simbols;
+	
+	for (int i = 0; simbols[i]; i++) {
+		if (simbols[i] == '0' || simbols[i] == '1' || simbols[i] == '2' || simbols[i] == '3' || simbols[i] == '4' || simbols[i] == '5' || simbols[i] == '6' || simbols[i] == '7' || simbols[i] == '8' || simbols[i] == '9') {
+			//
+		}
+		else {
+			std::cout << "Wrong number !!!\n";
+			goto up;
+		}
+	}
+	
+	array_size = atoi(simbols);
 	
 	array = new int[array_size];
 	
 	for (int i = 0; i < array_size; i++)
 	{
+		up_to:;
 		std::cout << "Enter " << i + 1 << " element : ";
-		std::cin >> array[i]; 
+		std::cin >> simbols; 
+		
+		for (int i = 0; simbols[i]; i++) {
+			if (simbols[i] == '0' || simbols[i] == '1' || simbols[i] == '2' || simbols[i] == '3' || simbols[i] == '4' || simbols[i] == '5' || simbols[i] == '6' || simbols[i] == '7' || simbols[i] == '8' || simbols[i] == '9') {
+				//
+			}
+			else {
+				std::cout << "Wrong number !!!\n";
+				goto up_to;
+			}
+		}
+		
+		array[i] = atoi(simbols);
+		
+		
 	}
 	
 	restore_array(array, array_size);
@@ -63,3 +94,4 @@ void show_array(int * array, int array_size)
 		std::cout << array[i] << ' ';
 	}
 }
+
